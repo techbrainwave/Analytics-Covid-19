9cc81b75000393c12228ee31f0893f53fa0fb8ee
Thomas, Anish <ash.m344@gmail.com>
2021-03-09 17:55:57 -0500

    Update data_preprocess.py

--- data_preprocess.py
+++ data_preprocess.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import datetime as dt
 
 # References:
 #   https://stackoverflow.com/questions/25224545/filtering-multiple-items-in-a-multi-index-python-panda-dataframe
@@ -10,28 +11,67 @@ def preprocess_data():
     Read all the individual files and combine into a single summarized file
     :return: N/A
     '''
+    date_format_in = "%m-%d-%Y"
 
-    # File locations
-    file_path_all = os.path.join("..\..\COVID-19-master\csse_covid_19_data\csse_covid_19_daily_reports", "01-01-2021.csv")
-    file_path_us  = os.path.join("..\..\COVID-19-master\csse_covid_19_data\csse_covid_19_daily_reports_us", "01-01-2021.csv")
-
-
-    # Read All
-    df_all = pd.read_csv(file_path_all,
-                         # index_col= [1,0],
-                         usecols = [2,3,4,7,8,9,10,12,13], # 'All' Template
-                         na_values=["nan"])
-
-    # Remove US and keep Rest, Set Index
-    df_rest = df_all[df_all.Country_Region.ne("US")] \
-              .set_index(["Country_Region","Province_State"])
+    df_rest = pd.DataFrame()
+    df_us   = pd.DataFrame()
 
 
-    # Read US
-    df_us = pd.read_csv(file_path_us,
-                         index_col=[1,0], # Set Index
-                         usecols=[0,1,2,5,6,7,8,10,13],    # 'US' Template
-                         na_values=["nan"])
+    path_all = os.fspath("..\..\COVID-19-master\csse_covid_19_data\csse_covid_19_daily_reports")
+    path_us  = os.fspath("..\..\COVID-19-master\csse_covid_19_data\csse_covid_19_daily_reports_us")
+
+    files_all = os.listdir(path=path_all)
+    files_us = os.listdir(path=path_us)
+
+
+    # Daily Reports
+    for file in files_all:
+        if file.endswith("21.csv"):
+
+            file_path_all = os.path.join(path_all,file)
+            date = dt.datetime.strptime(file.strip(".csv"),date_format_in).date()
+
+
+            # Read All
+            df_all = pd.read_csv(file_path_all,
+                                 # index_col= [1,0],
+                                 usecols=[2, 3, 4, 7, 8, 9, 10, 12, 13],  # 'All' Template
+                                 na_values=["nan"])
+            df_all['Date'] = date # Set Date
+
+
+            # Remove US and keep Rest, Set Indices
+            df_rest_tmp = df_all[df_all.Country_Region.ne("US")] \
+                        .set_index(["Country_Region","Province_State","Date"])
+
+
+            # Combine files
+            df_rest = df_rest\
+                     .append(df_rest_tmp, ignore_index=False)
+
+
+    # Daily Reports - US
+    for file in files_us:
+        if file.endswith(".csv"):
+
+            file_path_us = os.path.join(path_us,file)
+            date = dt.datetime.strptime(file.strip(".csv"),date_format_in).date()
+
+
+            # Read US
+            df_us_tmp = pd.read_csv(file_path_us,
+                                usecols=[0, 1, 2, 5, 6, 7, 8, 10, 13],  # 'US' Template
+                                na_values=["nan"])
+            df_us_tmp['Date'] = date # Set Date
+
+
+            # Set Indices
+            df_us_tmp = df_us_tmp.set_index(["Country_Region","Province_State","Date"])
+
+
+            # Combine files
+            df_us = df_us\
+                    .append(df_us_tmp, ignore_index=False)
 
 
     # Combine Rest & US
@@ -44,8 +84,7 @@ def preprocess_data():
 
 
     # Sort
-    df = df.sort_values(by=["Last_Update"],ascending=False)\
-        .sort_index(ascending=True)
+    df = df.sort_index(ascending=True)
     # print()
 
 
